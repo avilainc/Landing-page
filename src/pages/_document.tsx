d544ab262eeb17e30fede96cf98afb2e55359f2e
avilaops <nicolas@avilaops.com>
2026-02-08 15:50:43 +0000

    Add Google Analytics tracking (G-189DTP7YHB)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-189DTP7YHB"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-189DTP7YHB');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
